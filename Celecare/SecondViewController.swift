bc14da09fc07df5b95bb133caaf89cfda17038d0
Dominic Smith <doms230@live.com>
2017-06-21 12:50:13 -0500

    Initial Commit

---
+++ Celecare/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Celecare
+//
+//  Created by Mac Owner on 6/21/17.
+//  Copyright © 2017 Celecare LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
